427af2eb19e86c4d0fad622919006e02c5a6d7ad
minsoo0715 <minsoo0715@naver.com>
2021-03-22 10:06:23 +0900

    lambda, condition, io, loop

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace learn_cs
 {
@@ -6,27 +7,8 @@ namespace learn_cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); //출력함수 Console.WriteLine(string? value)
-
-            string name = "홍길동"; //var으로 타입 추론 가능
-            string message = $"My name is {name}"; // 보간 문자열
-
-            string input = Console.ReadLine(); //입력 받음 
-            Console.WriteLine($"input is {input}"); //보간 문자열은 일반 string 형과 동일하게 WriteLine 가능
-
-           Console.WriteLine(string.Format("My name is {0}", name)); //String.format("str", ...) 형식이고 {n}은 n-1번째 매개 변수를 뜻함..
-        /*
-        Ex) 
-        var A = 1;
-        var B = 2;
-        var C = 3;
-        String.format("{0} + {1} = {2}", A, B, C); 
-        output : "1 + 2 = 3"
-        */
-            //반복문 동일
-            for(int i = 0; i<10; i++) {
-                Console.WriteLine(i);
-            }
+            io.play_io();
+            lambda.play_Lambda();
         }
     }
 }
---
+++ io.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace learn_cs {
+    class io {
+        public static void play_io() {
+            Console.WriteLine("Hello World!"); //출력함수 Console.WriteLine(string? value)
+
+            string name = "홍길동"; //var으로 타입 추론 가능
+            string message = $"My name is {name}"; // 보간 문자열
+
+            string input = Console.ReadLine(); //입력 받음 
+            Console.WriteLine($"input is {input}"); //보간 문자열은 일반 string 형과 동일하게 WriteLine 가능
+
+           Console.WriteLine(string.Format("My name is {0}", name)); //String.format("str", ...) 형식이고 {n}은 n-1번째 매개 변수를 뜻함..
+        /*
+            Ex) 
+            var A = 1;
+            var B = 2;
+            var C = 3;
+            Console.WriteLine(String.format("{0} + {1} = {2}", A, B, C)); 
+            output : "1 + 2 = 3"
+        */
+            
+        }
+    }
+}
